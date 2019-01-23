b4e0bba3c2edee36d0368a96e009efb876372727
April Bingham <a.bingham@northeastern.edu>
2019-01-23 13:27:21 -0800

    adding component examples

---
+++ src/ClassComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ClassComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+ClassComponent.propTypes = {
+};
+
+export default ClassComponent;
---
+++ src/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FunctionalComponent = ({}) => (
+  <div></div>
+);
+
+FunctionalComponent.propTypes = {
+};
+
+export default FunctionalComponent;
