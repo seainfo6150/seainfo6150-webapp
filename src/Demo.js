cce25c6b85c80038f5c93d8b158dadbf35833f02
April Bingham <a.bingham@northeastern.edu>
2019-05-28 20:39:35 -0700

    cleanup

--- src/Demo.js
+++ src/Demo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import AddingMachine from './component-examples/AddingMachine.js';
-import HelloUser from './component-examples/HelloUser.js';
 
 const Demo = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ import './index.css';
 import App from './App';
 import Demo from "./Demo";
 
-import * as serviceWorker from './serviceWorker';
 const querystring = window.location.search || '';
-console.log('querystring', querystring);
 
 if (querystring.toLowerCase() === "?demo=true") {
     ReactDOM.render(<Demo />, document.getElementById('root'));
 } else {
     ReactDOM.render(<App />, document.getElementById('root'));
 }
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
