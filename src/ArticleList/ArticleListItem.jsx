12a46d7ec6f4d926031f98a4b09cf224f33f9842
Charusmitha <42979028+Charusmitha@users.noreply.github.com>
2019-06-13 17:26:00 -0700

    Updated article tag

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './ArticleListItem.module.css';
 
 const ArticleListItem = ({ title, author, pubDate, shortText }) => (
-  <div className={styles.articleListItem}>
+  <article className={styles.articleListItem}>
   	<h2 className={styles.articleTitle}>{title}</h2>
   	<p>{shortText}</p>
   	<p className={styles.articleDateAndAuthor}><cite>By: {author}</cite><br /><strong><time dateTime = {pubDate}>{pubDate}</time></strong></p>
-  </div>
+  </article>
 );
 
 ArticleListItem.propTypes = {
