b32259c21ab50bc4f6c6a7b22ddaaf9c96551d88
Kushagra Mall <bhattadmegha89@gmail.com>
2020-02-05 13:48:13 -0800

    Added changes to Assignment week-4

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 
 const ArticleListItem = ({articlelist})=>{ 
       return (
-        <html lang = "en">
-          <head>
-               <meta charset="UTF-8" />
-               <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-              <title>Article</title>
-          </head> 
-          <body>
-               <main>
+    
                   <section>   
                     <h2>{articlelist.title}</h2>
                     <p>{articlelist.shortText}</p>
@@ -24,9 +16,6 @@ const ArticleListItem = ({articlelist})=>{
                         </button>
                     </p>
                   </section>
-                  </main>
-                </body>
-          </html>
       );
     
 }
