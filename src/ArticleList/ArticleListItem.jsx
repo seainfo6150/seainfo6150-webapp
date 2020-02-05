34964692965902f1fa3f4ca1387a27816bebccf0
Kushagra Mall <bhattadmegha89@gmail.com>
2020-02-04 23:35:05 -0800

    Added changes to Week4 Assignment

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -3,16 +3,30 @@ import React from 'react'
 
 const ArticleListItem = ({articlelist})=>{ 
       return (
-        <section>
-            <h2>{articlelist.title}</h2>
-                <p>{articlelist.shortText}</p>
-                <p><b>Author:</b>{articlelist.author}</p>
-                <time dateTime={articlelist.pubYear}>
+        <html lang = "en">
+          <head>
+               <meta charset="UTF-8" />
+               <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+              <title>Article</title>
+          </head> 
+          <body>
+               <main>
+                  <section>   
+                    <h2>{articlelist.title}</h2>
+                    <p>{articlelist.shortText}</p>
+                    <p><b>Author:</b>{articlelist.author}</p>
+                    <time dateTime={articlelist.pubYear}>
                     {articlelist.pubDate}
-                </time>
-                <p><button onClick  ={() => alert(articlelist.slug)}>show article slug
-          </button></p>
-        </section>
+                    </time>
+                    <p>
+                        <button onClick = {() => alert(articlelist.slug)}>show article slug
+                        </button>
+                    </p>
+                  </section>
+                  </main>
+                </body>
+          </html>
       );
     
 }
