01a05ec1641b6b5041db42dd44d580ea50690141
April Bingham <a.bingham@northeastern.edu>
2020-08-05 15:53:57 -0700

    remove index.css import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
 import Demo from "./Demo";
 
