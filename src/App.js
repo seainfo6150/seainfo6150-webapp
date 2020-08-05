53370c09ed0cd9a381451f5b5d349e1dd23e2bfa
April Bingham <a.bingham@northeastern.edu>
2020-08-05 12:43:04 -0700

    add routes to App.js, update data fetching to hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Article from "./Article/Article";
 
 function App() {
+  let { url } = useRouteMatch();
+
   return (
     <div className="App">
-      <Article />
+      <Switch>
+        <Route path={`${url}/articlelist`}></Route>
+        <Route path={`${url}/articlelist/:slug`}></Route>
+        <Route>
+          <Article />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/component-examples/JsFundamentals/JsFundamentalsDataFetching.js
+++ src/component-examples/JsFundamentals/JsFundamentalsDataFetching.js
@@ -1,44 +1,45 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-export default class JsFundamentalsDataFetching extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-    };
-  }
+const JsFundamentalsDataFetching = () => {
+  const [fetchedData, setFetchedData] = useState([]);
 
-  async componentDidMount() {
-    // performs a GET request
-    const response = await fetch(
-      "http://dummy.restapiexample.com/api/v1/employees"
-    );
-    const responseJson = await response.json();
-    this.setState({ data: responseJson.data });
-  }
-
-  render() {
-    let displayContent;
-
-    if (this.state.data.length) {
-      displayContent = (
-        <ul>
-          {this.state.data.map((item) => (
-            <li key={item.id}>{item.employee_name}</li>
-          ))}
-        </ul>
+  useEffect(() => {
+    const fetchData = async () => {
+      // performs a GET request
+      const response = await fetch(
+        "http://dummy.restapiexample.com/api/v1/employees"
       );
-    } else {
-      displayContent = <div>You have no data!</div>;
-    }
+      const responseJson = await response.json();
+      setFetchedData(responseJson.data);
+    };
 
-    return (
-      <section>
-        <header>
-          <h2>Data fetching</h2>
-        </header>
-        {displayContent}
-      </section>
+    if (!fetchedData.length) {
+      fetchData();
+    }
+  }, [fetchedData]);
+
+  let displayContent;
+
+  if (fetchedData.length) {
+    displayContent = (
+      <ul>
+        {fetchedData.map((item) => (
+          <li key={item.id}>{item.employee_name}</li>
+        ))}
+      </ul>
     );
+  } else {
+    displayContent = <div>You have no data!</div>;
   }
-}
+
+  return (
+    <section>
+      <header>
+        <h2>Data fetching</h2>
+      </header>
+      {displayContent}
+    </section>
+  );
+};
+
+export default JsFundamentalsDataFetching;
