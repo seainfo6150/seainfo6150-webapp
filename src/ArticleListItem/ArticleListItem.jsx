fd96fb1b5d53106cca75f5bc829b219079ee75a3
001083544_yanlinliu <253939101@qq.com>
2020-11-01 17:26:50 -0800

    layout homework

--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -17,21 +17,26 @@ const ArticleListItem = ({ articles }) => {
 
   if (articles != null) {
     displayContent = (
-      <ul>
-        {keys.map((item, id) => (
-          <div key={id} className={styles.content}>
-            <h1>{articles[item].title}</h1>
-            <p>{btnState[id] ? articles[item].shortText : null}</p>
-            <p className={styles.date}>
-              {btnState[id] ? articles[item].displayDate : null}
-            </p>
-            <ArticleTextToggleButton
-              onClick={() => handleClick(id)}
-              buttonText={btnState[id] ? "Show Less" : "Show More"}
-            ></ArticleTextToggleButton>
+      <div className={styles.container}>
+        {keys.map((item, id) => {
+           return <div key={id} className={styles.content}>
+             <div className={styles.info}>
+              <h1>{articles[item].title}</h1>
+              <p>{btnState[id] ? articles[item].shortText : null}</p>
+              <p className={styles.date}>
+                {btnState[id] ? articles[item].displayDate : null}
+              </p>
+              </div>
+              <ArticleTextToggleButton
+              className={styles.btnContainer}
+                onClick={() => handleClick(id)}
+                buttonText={btnState[id] ? "Show Less" : "Show More"}
+              ></ArticleTextToggleButton>
+              
           </div>
-        ))}
-      </ul>
+
+        })}
+      </div>
     );
   } else {
     displayContent = <div>You have no data!</div>;
@@ -39,7 +44,6 @@ const ArticleListItem = ({ articles }) => {
 
   return (
     <section>
-      <header></header>
       {displayContent}
     </section>
   );
