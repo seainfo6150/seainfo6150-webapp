79551501218b2e1da06d2cd20253dfecbcb4942b
Phoebe Hu <phoebehu@phoebes-mbp.lan>
2021-02-26 18:26:54 -0800

    finish assignment

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import HTMLText from "../HTMLText/HTMLText";
+import React from 'react';
+import HTMLText from '../HTMLText/HTMLText';
 
 const DynamicArticle = (props) => {
+  const { title, author, authorEmail, displayDate, text } = props.article;
   return (
     <article>
       {/* add extra html with javascript attributes here */}
-      <HTMLText text={props.article.text} />
+      <header>
+        <h1>{title}</h1>
+        <address>
+          by {author} (<a href={authorEmail}>{authorEmail}</a>
+          )
+          <br />
+        </address>
+        <time dateTime={displayDate}>{displayDate}</time>
+      </header>
+      <HTMLText text={text} />
     </article>
   );
 };
