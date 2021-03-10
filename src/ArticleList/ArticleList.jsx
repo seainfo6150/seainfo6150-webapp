723f63705ea47ff7dc412281c97502515803e409
April Bingham <a.bingham@northeastern.edu>
2021-03-10 13:46:46 -0800

    final solution

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -3,23 +3,13 @@ import PropTypes from "prop-types";
 import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx";
 
 const ArticleList = (props) => {
-  let displayContent;
-
-  if (props.articles.length) {
-    displayContent = (
+  return (
+    <div>
       <ul>
         {props.articles.map((article) => (
           <ArticleListItem article={article} key={article.slug} />
         ))}
       </ul>
-    );
-  } else {
-    displayContent = <div>You have no data!</div>;
-  }
-
-  return (
-    <div>
-      {displayContent}
     </div>
   );
 };
--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import HTMLText from "../HTMLText/HTMLText";
+import HTMLText from "../HTMLText/HTMLText.jsx";
+import { Link } from "react-router-dom";
 
 const DynamicArticle = (props) => {
   return (
-    <article>
-      <header>
-        <h1>{props.article.title}</h1>
-        <address>
-          by {props.article.author} (
-          <a href={props.article.authorEmail}>{props.article.authorEmail}</a>)
-          <br />
-        </address>
-        <time dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
-      </header>
-      <HTMLText text={props.article.text} />
-    </article>
+    <div>
+      <Link to="/articlelist">Back</Link>
+      <article>
+        <header>
+          <h1>{props.article.title}</h1>
+          <address>
+            by {props.article.author} (
+            <a href={`mailto:${props.article.authorEmail}`}>
+              {props.article.authorEmail}
+            </a>
+            )
+            <br />
+          </address>
+          <time dateTime={props.article.timeStamp}>
+            {props.article.displayDate}
+          </time>
+        </header>
+        <HTMLText text={props.article.text} />
+      </article>
+    </div>
   );
 };
 
